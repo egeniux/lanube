54897cf697abfa8325cd0024431cd3e2886b83f9
LANUBE <lanubecontrol@gmail.com>
2019-01-15 15:04:17 +0100

    add login users functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,6 +15,11 @@ router.get('/test', (req, res) => res.json({
   msg: 'Users Works'
 }));
 
+// @route GET api/users
+// @desc Register users route
+// @access Public
+
+
 router.post('/register', (req, res) => {
   User.findOne({
       email: req.body.email
@@ -46,9 +51,38 @@ router.post('/register', (req, res) => {
     })
 });
 
-// @route GET api/users
-// @desc Register users route
+// @route GET api/users/login
+// @desc Login User / Returning JWT Token
 // @access Public
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({
+      email
+    })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({
+          email: 'User not found'
+        });
+      }
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({
+              msg: 'Success'
+            });
+          } else {
+            return res.status(400).json({
+              password: 'Password incorrect'
+            });
+          }
+        })
+    })
+});
 
 
 
