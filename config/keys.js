56fcd094ecdeac4466ee7089f995f7a42d32cb16
LANUBE <lanubecontrol@gmail.com>
2019-01-15 15:17:50 +0100

    add token functionality

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://lanube:Rty56t7!1@ds157204.mlab.com:57204/lanube'
+  mongoURI: 'mongodb://lanube:Rty56t7!1@ds157204.mlab.com:57204/lanube',
+  secretOrKey: 'lanube'
 }
